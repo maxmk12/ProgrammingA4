df668bd3493abcbb990c4f376a8e99395b677fb9
Max Carr <maxcarr12@gmail.com>
2018-04-23 18:48:41 -0600

    Initial Upload

---
+++ src/ProgrammingA4/ProgrammingA4.java
@@ -0,0 +1,40 @@
+package ProgrammingA4;
+
+/*
+* Author: Max Carr, Erin Cook, Zac Sliker
+* Date: 4/13/2018 
+* Overview: Runs a Dynamic Algorithm to determine lowest possible number of coins needed to get the total.
+*/
+
+public class ProgrammingA4 {
+	public static int p(int[] change, int a) {
+////////////////////////////////////Checking for array of 0's//////////////////////////////////////////////
+                int p = 0;
+                for(int k = 0; k < change.length; k++){
+                    p += change[k];
+                }
+                if(p == 0){
+                    throw new IllegalArgumentException("change of size 0 is not allowed");
+                }
+///////////////////////////////Initialization of T and Result Arrays//////////////////////////////////
+		int[] Result = new int[a + 1];
+                int[] T = new int[a + 1];
+		T[0] = 0;
+		for (int i = 1; i <= a; i++) {
+			T[i] = Integer.MAX_VALUE-1;
+			Result[i] = -1;
+		}
+///////////////////////////////////Dynamic Algorithm to solve/////////////////////////////////////
+		for (int j = 0; j < change.length; j++){
+			for (int i = 1; i <= a; i++) {
+				if (i >= change[j]) {
+					if (T[i - change[j]] + 1 < T[i]) {
+						T[i] = 1 + T[i - change[j]];
+						Result[i] = j;
+					}
+				}
+			}
+                }
+		return T[a];
+	}
+}
---
+++ test/ProgrammingA4/ProgrammingA4Test.java
@@ -0,0 +1,48 @@
+package ProgrammingA4;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+* Author: Max Carr, Erin Cook, Zac Sliker
+* Date: 4/13/2018 
+* Overview: Tests the ProgrammingA4.java file with 5 tests, if the array is all 0's
+* if the array is empty, if the array is american coins and a total value of 50,
+* if the array is american coins and has a total value of 73, and if the array 
+* are not american and the total value is 46.
+*/
+
+public class ProgrammingA4Test {
+    @Test(expected = IllegalArgumentException.class)
+    public void test1(){
+        int array[] = {0,0,0,0};
+        int total = 0;
+	Assert.assertEquals(0, ProgrammingA4.p(array, total));
+    }   
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void test2(){
+        int array[] = {};
+        int total = 0;
+	Assert.assertEquals(0, ProgrammingA4.p(array, total));
+    }
+    @Test
+    public void test3(){
+        int array[] = {25,10,5,1};
+        int total = 50;
+        int expected[] = {25,25};
+        Assert.assertEquals(2, ProgrammingA4.p(array, total));
+    }
+    @Test
+    public void test4(){
+        int array[] = {7,2,4,6};
+        int total = 13;
+        Assert.assertEquals(2, ProgrammingA4.p(array, total));
+    }
+    @Test
+    public void test5(){
+        int array[] = {8,6,5,1};
+        int total = 11;
+        Assert.assertEquals(2, ProgrammingA4.p(array, total));
+    }
+}
